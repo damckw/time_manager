dd37acebcbd5821c686433ab6d917fff8667ae52
William Choukroun <42743593+WilliamCkr@users.noreply.github.com>
2021-11-02 17:29:22 +0100

    mocup create

---
+++ Front/src/components/test.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="container">
+  <p id="countdown"></p>
+</div>
+
+</template>
+
+<script>
+setInterval(myTimer, 1000);
+
+function myTimer() {
+  const d = new Date();
+  document.getElementById("countdown").innerHTML = d.toLocaleTimeString();
+}
+</script>
